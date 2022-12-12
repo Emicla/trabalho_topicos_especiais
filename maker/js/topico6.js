da642381c7dfa26868a656a4f6be78673d8be93f
Emiclã Lenon da Rosa <emiclalenondarosa@gmail.com>
2022-12-11 23:21:23 -0300

    Finalizado estilização

--- maker/js/topico6.js
+++ maker/js/topico6.js
@@ -6,3 +6,13 @@ function mudarTexto(tag) {
         tag.textContent = "Click aqui";
     }
 }
+
+function mostrarDados(tag) {
+    tag.classList.add("invisivel");
+    tag.parentElement.parentElement.children[1].classList.remove("invisivel");
+}
+
+function esconderDados(tag){
+    tag.parentElement.children[1].classList.remove("invisivel");
+    tag.parentElement.parentElement.children[1].classList.add("invisivel");
+}
